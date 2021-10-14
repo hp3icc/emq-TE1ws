0e268575ee7546e13d1a11c41d0d26bc5956200c
hp3icc <49044505+hp3icc@users.noreply.github.com>
2021-10-13 23:16:29 -0500

    Update emq-TE1ws-Rev11.sh

--- emq-TE1ws-Rev11.sh
+++ emq-TE1ws-Rev11.sh
@@ -459,7 +459,7 @@ sudo cat > /bin/menu <<- "EOF"
 
 while : ; do
 
-choix=$(whiptail --title "TE1ws-Rev10e Raspbian Proyect HP3ICC Esteban Mackay 73." --menu "Suba o Baje con las flechas del teclado y seleccione el numero de opcion:" 24 58 15 \
+choix=$(whiptail --title "TE1ws-Rev11 Raspbian Proyect HP3ICC Esteban Mackay 73." --menu "Suba o Baje con las flechas del teclado y seleccione el numero de opcion:" 24 58 15 \
 1 " APRS Direwolf Analogo" \
 2 " APRS Direwolf RTL-SDR " \
 3 " APRS Multimon-ng " \
