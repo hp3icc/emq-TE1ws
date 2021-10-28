3b4aed7efa9d8d5301d455691cbe9648b38aae78
hp3icc <49044505+hp3icc@users.noreply.github.com>
2021-10-28 01:31:02 -0500

    Update x.sh

--- x.sh
+++ x.sh
@@ -1,133 +1,70 @@
 #!/bin/bash
-# A tool to install xlxd, your own D-Star Reflector.
-# For more information, please visit: https://n5amd.com
-#Lets begin-------------------------------------------------------------------------------------------------
-WHO=$(whoami)
-if [ "$WHO" != "root" ]
-then
-  echo ""
-  echo "You Must be root to run this script!!"
-  exit 0
-fi
-if [ ! -e "/etc/debian_version" ]
-then
-  echo ""
-  echo "This script is only tested in Debian 9 and x64 cpu Arch. "
-  exit 0
-fi
-DIRDIR=$(pwd)
-LOCAL_IP=$(ip a | grep inet | grep "eth0\|en" | awk '{print $2}' | tr '/' ' ' | awk '{print $1}')
-INFREF=https://n5amd.com/digital-radio-how-tos/create-xlx-xrf-d-star-reflector/
-XLXDREPO=https://github.com/LX3JL/xlxd.git
-DMRIDURL=http://xlxapi.rlx.lu/api/exportdmr.php
-WEBDIR=/var/www/xlxd
-XLXINSTDIR=/root/reflector-install-files/xlxd
-DEP="git build-essential apache2 php libapache2-mod-php php7.0-mbstring"
-DEP2="git build-essential apache2 php libapache2-mod-php php7.3-mbstring"
-VERSION=$(sed 's/\..*//' /etc/debian_version)
-clear
+# apt-get update
+# apt-get upgrade
 
-mkdir -p $XLXINSTDIR
-mkdir -p $WEBDIR
-apt-get update
-if [ $VERSION = 9 ]
-then
-    apt-get -y install $DEP
-    a2enmod php7.0
-elif [ $VERSION = 10 ]
-then
-    apt-get -y install $DEP2
-fi
+## Install Git
 
-echo "------------------------------------------------------------------------------"
-if [ -e $XLXINSTDIR/xlxd/src/xlxd ]
-then
-   echo ""
-   echo "It looks like you have already compiled XLXD. If you want to install/complile xlxd again, delete the directory '/root/reflector-install-files/xlxd' and run this script again. "
-   exit 0
-else
-   echo "Downloading and compiling xlxd... "
-   echo "------------------------------------------------------------------------------"
-   cd $XLXINSTDIR
-   git clone $XLXDREPO
-   cd $XLXINSTDIR/xlxd/src
-   make clean
-   make
-   make install
-fi
-if [ -e $XLXINSTDIR/xlxd/src/xlxd ]
-then
-   echo ""
-   echo ""
-   echo "------------------------------------------------------------------------------"
-   echo "It looks like everything compiled successfully. There is a 'xlxd' application file. "
-else
-   echo ""
-   echo "UH OH!! I dont see the xlxd application file after attempting to compile."
-   echo "The output above is the only indication as to why it might have failed.  "
-   echo "Delete the directory '/root/reflector-install-files/xlxd' and run this script again. "
-   echo ""
-   exit 0
-fi
-echo "------------------------------------------------------------------------------"
-echo "Getting the DMRID.dat file... "
-echo "------------------------------------------------------------------------------"
-wget -O /xlxd/dmrid.dat $DMRIDURL
-echo "------------------------------------------------------------------------------"
-echo "Copying web dashboard files and updating init script... "
-cp -R $XLXINSTDIR/xlxd/dashboard/* /var/www/xlxd/
-cp $XLXINSTDIR/xlxd/scripts/xlxd /etc/init.d/xlxd
-sed -i "s/XLX999 192.168.1.240 127.0.0.1/XLX744 0.0.0.0 127.0.0.1/g" /etc/init.d/xlxd
+apt-get install git git-core -y
+
+## Install webserver with PHP5 support
+
+apt-get install apache2 php5 -y
+
+## Install g++ compiler
+
+apt-get install build-essential -y
+##(skip this step on Debian 8.x) 
+apt-get install g++-4.7 -y
+
+
+## After downloading and before compiling, please have a look at the main.h file
+
+## nano /xlxd/src/main.h
+
+## For YSF you need to define a default HotSpot frequency !!!
+
+## Download and compile the XLX sources
+
+git clone https://github.com/LX3JL/xlxd.git
+cd xlxd/src/
+make clean
+make
+make install
+
+## Copy startup script "xlxd" to /etc/init.d
+
+cp ~/xlxd/scripts/xlxd /etc/init.d/xlxd
+
+## Adapt the default startup parameters to your needs
+
+# pico /etc/init.d/xlxd
+
+## Download the dmrid.dat from the XLXAPI server to your xlxd folder
+
+wget -O /xlxd/dmrid.dat http://xlxapi.rlx.lu/api/exportdmr.php
+
+## Check your FTDI driver and install the AMBE service according to the readme in AMBEd
+
+ 
+
+## Last step is to declare the service for automatic startup and shutdown
+
 update-rc.d xlxd defaults
-# Delaying startup time
-mv /etc/rc3.d/S01xlxd /etc/rc3.d/S10xlxd
-echo "Updating XLXD Config file... "
-XLXCONFIG=/var/www/xlxd/pgs/config.inc.php
-sed -i "s/your_email/setcom40@gmail.com/g" $XLXCONFIG
-sed -i "s/LX1IQ/HP3ICC/g" $XLXCONFIG
-sed -i "s/http:\/\/your_dashboard/http:\/\/localhost/g" $XLXCONFIG
-sed -i "s/\/tmp\/callinghome.php/\/xlxd\/callinghome.php/g" $XLXCONFIG
-echo "Copying directives and reloading apache... "
-cp $DIRDIR/templates/apache.tbd.conf /etc/apache2/sites-available/$XLXDOMAIN.conf
-sed -i "s/apache.tbd/$XLXDOMAIN/g" /etc/apache2/sites-available/$XLXDOMAIN.conf
-sed -i "s/ysf-xlxd/xlxd/g" /etc/apache2/sites-available/localhost.conf
-chown -R www-data:www-data /var/www/xlxd/
-chown -R www-data:www-data /xlxd/
-a2ensite $XLXDOMAIN
-sed -i "s/CallingHome\['Active'\]                               = false/CallingHome\['Active'\]                               = true/g" /var/www/xlxd/pgs/config.inc.php
-service xlxd start
-systemctl restart apache2
-echo "------------------------------------------------------------------------------"
-echo ""
-echo ""
-echo "******************************************************************************"
-echo ""
-echo ""
-echo "XLXD is finished installing and ready to be used. Please read the following..."
-echo ""
-echo ""
-echo "******************************************************************************"
-echo ""
-echo " For Public Reflectors: "
-echo "If your XLX number is not already taken, enabling callinghome is all you need to do  "
-echo "for your reflector to be added to all the host files automatically. It does take     "
-echo "about an hour for the change to reflect, if your reflector is accessible and working."
-echo "Once activated, the callinghome hash to backup will be /xlxd/callinghome.php. "
-echo "More Information: $INFREF"
-echo ""
-echo ""
-echo " For test/private Reflectors: "
-echo "If you are using this reflector as a test or for offline access you will  "
-echo "need to configure the host files of the devices connecting to this server."
-echo "There are many online tutorials on 'Editing pi-star host files'.          "
-echo ""
-echo ""
-echo "          Your $XRFNUM dashboad should now be accessible...            "
-echo "                http://localhost/                                      "
-echo ""
-echo ""
-echo "You can make further customizations to the main config file $XLXCONFIG."
-echo "Be sure to thank the creators of xlxd for the ability to spin up          "
-echo "your very own D-Star reflector.                                           "
-echo ""
-echo "------------------------------------------------------------------------------"
+
+## Start or stop the service with
+
+# service xlxd start
+# service xlxd stop
+
+## Copy dashboard to /var/www
+
+cp -r ~/xlxd/dashboard /var/www/db
+
+## Give the dashboard read access to the server log file
+
+chmod +r /var/log/messages 
+
+## Reboot server to see if the auto-start is working
+
+# reboot
+
