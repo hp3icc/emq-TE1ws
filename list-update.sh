27769bd95f32e5ea3127fa01cad3609b8ba03e2d
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-04-17 18:48:02 -0500

    Update list-update.sh

--- list-update.sh
+++ list-update.sh
@@ -1,72 +1,3 @@
-emq=18
-sudo cat > /bin/menu <<- "EOF"
-#!/bin/bash
-while : ; do
-choix=$(whiptail --title "TE1ws-Rev: R00ab UPDATE/ Raspbian Proyect HP3ICC Esteban Mackay 73." --menu "Suba o Baje con las flechas del teclado y seleccione el numero de opcion:" 24 67 15 \
-1 " APRS Direwolf Analogo" \
-2 " APRS Direwolf RTL-SDR " \
-3 " APRS Multimon-ng " \
-4 " APRS Ionosphere " \
-5 " MMDVMHost " \
-6 " Dvswitch " \
-7 " pYSFReflector3 " \
-8 " YSF2DMR " \
-9 " FreeDMR Server " \
-10 " Editar WiFi " \
-11 " DDNS NoIP " \
-12 " GoTTY " \
-13 " actualizar " \
-14 " Reiniciar Equipo " \
-15 " Salir del menu " 3>&1 1>&2 2>&3)
-exitstatus=$?
-#on recupere ce choix
-#exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    echo "Your chosen option:" $choix
-else
-    echo "You chose cancel."; break;
-fi
-# case : action en fonction du choix
-case $choix in
-1)
-menu-dw-analogo;;
-2)
-menu-dw-rtl;;
-3)
-menu-mm-rtl;;
-4)
-menu-ionos;;
-5)
-menu-mmdvm;;
-6)
-menu-dvs;;
-7)
-menu-ysf;;
-8)
-menu-ysf2dmr;;
-9)
-menu-fdmr;;
-10)
-menu-wifi;;
-11)
-menu-noip ;;
-12)
-menu-web ;;
-13)
-menu-update ;;
-14)
-menu-reboot ;;
-15)
-break;
-esac
-done
-exit 0
-EOF
-#
-sudo sed -i "s/R00ab/$emq/g"  /bin/menu
-rm /bin/MENU
-cp /bin/menu /bin/MENU
-#
 sudo cat > /bin/menu-update <<- "EOF"
 #!/bin/bash
 while : ; do
@@ -100,5 +31,4 @@ exit 0
 EOF
 #
 chmod +x /bin/menu*
-chmod +x /bin/MENU
 menu-update
