33ce586e967c14000b66b5d26ea3fa87336e4c60
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-04-18 00:19:51 -0500

    Update list-update.sh

--- list-update.sh
+++ list-update.sh
@@ -67,6 +67,74 @@ sudo sed -i "s/R00ab/$variable22/g"  /bin/menu
 rm /bin/MENU
 cp /bin/menu /bin/MENU
 #
+sudo cat > /bin/menu-up-fdm <<- "EOF"
+#!/bin/bash
+while : ; do
+choix=$(whiptail --title "Raspbian Proyect HP3ICC / update FreeDMR" --menu "Nota Importante: debe debe agregar todos sus obp en la opcion numero uno, ( 1-Lista de OBP )antes de iniciar la actualizacion, el proceso de actualizacion borrara por completo la carpeta /opt/FreeDMR, al finalizar la actualizacion el servicio Freedmr se reinniciara automaticamente.
+" 17 50 4 \
+1 " List OBP " \
+2 " List Rules  " \
+3 " shell extra " \
+4 " Iniciar Actualizacion FreeDMR  " \
+5 " Menu Principal " 3>&1 1>&2 2>&3)
+exitstatus=$?
+#on recupere ce choix
+#exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $choix
+else
+    echo "You chose cancel."; break;
+fi
+# case : action en fonction du choix
+case $choix in
+1)
+sudo nano /opt/obp.txt ;;
+2)
+sudo nano /opt/rules.txt ;;
+3)
+sudo nano /opt/extra-1.sh && chmod +x /opt/extra* ;;
+4)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/fdmr-update.sh)" ;;
+5)
+break;
+esac
+done
+exit 0
+
+
+EOF
+#
+sudo cat > /bin/menu-up-hbmon2 <<- "EOF"
+#!/bin/bash
+while : ; do
+choix=$(whiptail --title "Raspbian Proyect HP3ICC / update HBMon2" --menu "Nota Importante: antes de iniciar la actualizacion, el proceso de actualizacion borrara por completo todas las configuraciones, al finalizar la actualizacion el servicio se reinniciara automaticamente.
+" 17 50 4 \
+1 " shell extra " \
+2 " Iniciar Actualizacion HBMon2  " \
+3 " Menu Principal " 3>&1 1>&2 2>&3)
+exitstatus=$?
+#on recupere ce choix
+#exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $choix
+else
+    echo "You chose cancel."; break;
+fi
+# case : action en fonction du choix
+case $choix in
+1)
+sudo nano /opt/extra-2.sh && chmod +x /opt/extra* ;;
+2)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/hbmon2-update.sh)" ;;
+3)
+break;
+esac
+done
+exit 0
+
+
+EOF
+#
 sudo cat > /bin/menu-update <<- "EOF"
 #!/bin/bash
 while : ; do
@@ -87,7 +155,7 @@ fi
 # case : action en fonction du choix
 case $choix in
 1)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/hbmon2-update.sh)" ;;
+menu-up-hbmon2 ;;
 2)
 menu-up-fdm ;;
 3)
@@ -97,6 +165,7 @@ break;
 esac
 done
 exit 0
+
 EOF
 #
 chmod +x /bin/menu*
