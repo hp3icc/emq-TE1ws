0c3eb64b2d8c06ba61979980b4a99d2db8f775c8
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-04-17 22:15:56 -0500

    Update emq-TE1x.sh

--- emq-TE1x.sh
+++ emq-TE1x.sh
@@ -2909,19 +2909,20 @@ EOF
 sudo cat > /opt/rules.txt <<- "EOF"
  
 BRIDGES = {
-
+ 
  '9990': [ 
 {'SYSTEM': 'EchoTest',          'TS': 2, 'TGID': 9990, 'ACTIVE':True, 'TIMEOUT': 0, 'TO_TYPE': 'NONE', 'ON': [], 'OFF': [], 'RESET': []}, 
-
+ 
 ],
-
-
+ 
+ 
+ 
 }
 if __name__ == '__main__':
     from pprint import pprint
     pprint(BRIDGES)
-
  
+  
  
 EOF
 ###################
