15d47ad8662207c06725e2a72970cfb2bb7669b1
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-04-17 19:53:58 -0500

    Update list-update.sh

--- list-update.sh
+++ list-update.sh
@@ -93,7 +93,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/fdmr
 3)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/list-update.sh)" ;;
 4)
-/bin/menu;
+break && /bin/menu;
 esac
 done
 exit 0
