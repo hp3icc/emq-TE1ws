5aa02649bca66407c2d67929c1acc34ec3154345
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-05-18 23:00:57 -0500

    Update emq-TE1x-AP.sh

--- emq-TE1x-AP.sh
+++ emq-TE1x-AP.sh
@@ -1016,7 +1016,7 @@ sudo cat > /usr/local/bin/rebooter1.sh <<- "EOF"
 sleep 180
 while :
 do
-SERVER=8.8.4.4
+SERVER=noip.com
 
 ping -c1 ${SERVER} > /dev/null
 
@@ -1223,7 +1223,7 @@ RestartSec=3
 StandardOutput=null
 WorkingDirectory=/opt/ionsphere/ionosphere-raspberry-pi
 #ExecStartPre=/bin/sleep 30
-ExecStartPre=/bin/sh -c 'until ping -c1 google.com; do sleep 1; done;'
+ExecStartPre=/bin/sh -c 'until ping -c1 noip.com; do sleep 1; done;'
 ExecStart=sh /opt/ionsphere/ionosphere-raspberry-pi/ionos.sh
 ExecReload=/bin/kill -HUP $MAINPID
 KillMode=process
@@ -1258,7 +1258,7 @@ After=syslog.target network-online.target
 [Service]
 User=root
 #ExecStartPre=/bin/sleep 1800
-ExecStartPre=/bin/sh -c 'until ping -c1 google.com; do sleep 1; done;'
+ExecStartPre=/bin/sh -c 'until ping -c1 noip.com; do sleep 1; done;'
 ExecStart=/opt/MMDVMHost/DMRIDUpdate.sh
 
 [Install]
@@ -1279,7 +1279,7 @@ RestartSec=3
 StandardOutput=null
 WorkingDirectory=/opt/MMDVMHost
 #ExecStartPre=/bin/sleep 10
-ExecStartPre=/bin/sh -c 'until ping -c1 google.com; do sleep 1; done;'
+ExecStartPre=/bin/sh -c 'until ping -c1 noip.com; do sleep 1; done;'
 ExecStart=/opt/MMDVMHost/MMDVMHost /opt/MMDVMHost/MMDVM.ini
 ExecStop=/usr/bin/screen -S MMDVMHost -X quit
 
@@ -1302,7 +1302,7 @@ RestartSec=3
 StandardOutput=null
 WorkingDirectory=/opt/DMRGateway
 #ExecStartPre=/bin/sleep 10
-ExecStartPre=/bin/sh -c 'until ping -c1 google.com; do sleep 1; done;'
+ExecStartPre=/bin/sh -c 'until ping -c1 noip.com; do sleep 1; done;'
 ExecStart=/usr/local/bin/DMRGateway /opt/DMRGateway/DMRGateway.ini
 ExecStop=/usr/bin/screen -S DMRGateway -X quit
  
@@ -3348,7 +3348,7 @@ rm /var/log/syslog*
 rm /var/log/*.log*
 cd /tmp/
 
-(crontab -l; echo "* */1 * * * sudo sync ; echo 3 > /proc/sys/vm/drop_caches >/dev/null 2>&1")|awk '!x[$0]++'|crontab -
+(crontab -l; echo "* */1 * * * sync ; echo 3 > /proc/sys/vm/drop_caches >/dev/null 2>&1")|awk '!x[$0]++'|crontab -
 #####
 cat > /tmp/completado.sh <<- "EOF"
 #!/bin/bash
