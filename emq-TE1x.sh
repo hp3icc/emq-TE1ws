6c73a6cc626be0a224414d2d58c80eba5f528770
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-05-19 14:39:15 -0500

    Update emq-TE1x.sh

--- emq-TE1x.sh
+++ emq-TE1x.sh
@@ -2764,53 +2764,31 @@ WantedBy=multi-user.target
 EOF
 #######
 cd /opt
-git clone https://github.com/yuvelq/FDMR-Monitor.git
-mv /opt/FDMR-Monitor /opt/HBMonv2
-cd HBMonv2
+sudo git clone https://github.com/yuvelq/FDMR-Monitor
+cd FDMR-Monitor
 sudo chmod +x install.sh
 sudo ./install.sh
-cp config_SAMPLE.py config.py
-sudo chmod +x /opt/HBMonv2/monitor.py
-sudo sed -i 's/FREQUENCY       = 10/FREQUENCY       = 60/' /opt/HBMonv2/config.py
+sudo cp fdmr-mon_SAMPLE.cfg fdmr-mon.cfg
+sudo chmod 644 fdmr-mon.cfg
+sudo cp utils/logrotate/fdmr_mon /etc/logrotate.d/
+sudo cp utils/systemd/fdmr_mon.service /lib/systemd/system/
+#sudo sed -i 's/FREQUENCY = 10/FREQUENCY = 60/' /opt/FDMR-Monitor/fdmr-mon.cfg
 #
-sudo cat > /opt/HBMonv2/updateTGIDS.sh <<- "EOF"
-#!/bin/bash
 
-#sleep $[ ( $RANDOM % 1800 )  + 1 ]s
-
-cd /opt/HBMonv2
-FILE_NAME="talkgroup_ids.json"
-#FETCH_URL="http://downloads.freedmr.uk/downloads/talkgroup_ids.json"
-FETCH_URL="https://freedmr.cymru/talkgroups/talkgroup_ids_json.php"
-
-OLD_MD5=`md5sum ${FILE_NAME}`
-wget -O ${FILE_NAME} ${FETCH_URL}
-NEW_MD5=`md5sum ${FILE_NAME}`
-
-if [ "$NEW_MD5" != "$OLD_MD5" ]
-then
-  echo "File is new, restarting"
-  sync ; echo 3 > /proc/sys/vm/drop_caches && sudo systemctl restart hbmon2.service
-fi
-
-EOF
-cp /opt/HBMonv2/updateTGIDS.sh /opt/HBmonitor
-sudo sed -i 's/HBMonv2/HBmonitor/' /opt/HBmonitor/updateTGIDS.sh
-sudo sed -i 's/hbmon2/hbmon/' /opt/HBmonitor/updateTGIDS.sh
 ####
-sudo sed -i 's/localhost_2-day.png/localhost_1-day.png/' /opt/HBMonv2/html/sysinfo.php
-cd /opt/HBMonv2/sysinfo/
-sudo sed -i 's/var\/www\/html/opt\/HBMonv2\/html/' cpu.sh
-sudo sed -i 's/var\/www\/html/opt\/HBMonv2\/html/' graph.sh
+sudo sed -i 's/localhost_2-day.png/localhost_1-day.png/' /opt/FDMR-Monitor/html/sysinfo.php
+cd /opt/FDMR-Monitor/sysinfo/
+sudo sed -i 's/var\/www\/html/opt\/FDMR-Monitor\/html/' cpu.sh
+sudo sed -i 's/var\/www\/html/opt\/FDMR-Monitor\/html/' graph.sh
 
-sudo chmod +x /opt/HBMonv2/sysinfo/cpu.sh
-sudo chmod +x /opt/HBMonv2/sysinfo/graph.sh
-sudo chmod +x /opt/HBMonv2/sysinfo/rrd-db.sh
-sudo sh /opt/HBMonv2/sysinfo/rrd-db.sh
-sudo chmod +x /opt/HBMonv2/updateTGIDS.sh
-sudo chmod +x /opt/HBmonitor/updateTGIDS.sh
+sudo chmod +x /opt/FDMR-Monitor/sysinfo/cpu.sh
+sudo chmod +x /opt/FDMR-Monitor/sysinfo/graph.sh
+sudo chmod +x /opt/FDMR-Monitor/sysinfo/rrd-db.sh
+sudo sh /opt/FDMR-Monitor/sysinfo/rrd-db.sh
+sudo chmod +x /opt/FDMR-Monitor/updateTGIDS.sh
 
-sudo cat > /opt/HBMonv2/html/buttons.html <<- "EOF"
+
+sudo cat > /opt/FDMR-Monitor/html/buttons.php <<- "EOF"
 <!-- HBMonitor buttons HTML code -->
 <a class="button"  href="index.php">&nbsp;Home&nbsp;</a>
 <!--
@@ -2849,6 +2827,9 @@ sudo cat > /opt/HBMonv2/html/buttons.html <<- "EOF"
 <a class="button" href="sysinfo.php">&nbsp;System Info&nbsp;</a>
 &nbsp;
 <a class="button" href="log.php">&nbsp;Lastheard&nbsp;</a>
+&nbsp;
+<a class="button" href="tgcount.php">&nbsp;TG Count&nbsp;</a>
+&nbsp;
 </div>
 </div>
 &nbsp;
@@ -2895,34 +2876,7 @@ sudo cat > /opt/HBMonv2/html/buttons.html <<- "EOF"
 </div>
 -->
 EOF
-###
-
-sudo cat > /lib/systemd/system/hbmon2.service <<- "EOF"
-[Unit]
-Description=HBMonitor2
-# To make the network-online.target available
-# systemctl enable systemd-networkd-wait-online.service
-
-After=network-online.target syslog.target
-Wants=network-online.target
-
-[Service]
-User=root
-Type=simple
-Restart=always
-RestartSec=3
-StandardOutput=null
-WorkingDirectory=/opt/HBMonv2
-ExecStart=/usr/bin/python3 /opt/HBMonv2/monitor.py
-
-
-[Install]
-WantedBy=multi-user.target
-
-EOF
-###################
-
-######
+#########
 sudo cat > /opt/FreeDMR/config/rules.py <<- "EOF"
 '''
 THIS EXAMPLE WILL NOT WORK AS IT IS - YOU MUST SPECIFY YOUR OWN VALUES!!!
