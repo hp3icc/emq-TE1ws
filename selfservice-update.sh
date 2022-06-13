ca4f14103158dbff7590c59d77f67f34dc43e00e
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-06-12 22:52:48 -0500

    Update selfservice-update.sh

--- selfservice-update.sh
+++ selfservice-update.sh
@@ -7,7 +7,7 @@ rm -r /opt/FDMR-Monitor/
 sudo apt install -y apache2 apache2-utils
 systemctl restart apache2
 sudo systemctl enable apache2
-sudo apt install mariadb-server mariadb-client
+sudo apt install mariadb-server mariadb-client -y
 systemctl restart mariadb
 systemctl enable mariadb
 sudo mysql_secure_installation
