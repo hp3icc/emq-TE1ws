4a30a6636f1aeb32be70730f3c4af7c3c308bde4
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-06-13 23:16:01 -0500

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 sudo apt install -y apache2 apache2-utils
-systemctl restart apache2
-sudo systemctl enable apache2
+systemctl stop apache2
+sudo systemctl disable apache2
 sudo apt install mariadb-server mariadb-client -y
 systemctl restart mariadb
 systemctl enable mariadb
