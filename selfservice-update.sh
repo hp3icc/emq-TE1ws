c338f12ef3c059afe66f5d2bd779bfe112e0ba98
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-06-13 21:24:32 -0500

    Update selfservice-update.sh

--- selfservice-update.sh
+++ selfservice-update.sh
@@ -134,127 +134,9 @@ sudo systemctl daemon-reload
 sudo chmod +x /opt/extra-2.sh
 sudo sh /opt/extra-2.sh
 cd /opt/FDMR-Monitor
-#sudo ./install.sh
+sudo ./install.sh
 #####################
-sudo cat > /opt/FDMR-Monitor/install.sh <<- "EOF"
-#! /bin/bash
 
-# Install the required support programs
-apt install python3 python3-pip python3-dev libffi-dev libssl-dev cargo sed \
-default-libmysqlclient-dev build-essential -y
-pip3 install -r requirements.txt
-
-# fdmr-mon folder
-mon_path=$(pwd)/
-# Copy config file 
-if [ ! -e fdmr-mon.cfg  ]; then
-  if [ -e fdmr-mon_SAMPLE.cfg  ]; then
-    cp /opt/FDMR-Monitor/fdmr-mon_SAMPLE.cfg /opt/FDMR-Monitor/fdmr-mon.cfg
-    echo 'Config file copied successfully.'
-  else
-    echo 'fdmr-mon_SAMPLE.cfg not found, exiting.'
-    exit 1
-  fi
-else
-  echo 'Config file alredy exists.'
-fi
-# Create a new branch and copy proxy files
-read -p 'Do you want to create a new FreeDMR branch and copy the Proxy files into it? [Y/n]: ' branch
-branch=${branch:-y}
-if [ "${branch,,}" == 'y' ]; then
-  valid=''
-  for i in {1..3}; do
-    read -p 'Insert the path to FreeDMR folder e.g. /opt/FreeDMR/: ' p2fdmr
-    if [ -z "$p2fdmr" ] | [ ! -d "$p2fdmr" ]; then
-      echo "Path to FreeDMR folder: '${p2fdmr}' not found, try again."
-      if [ $i -eq 3 ]; then
-        echo 'To many errors, skipping this.'
-      fi
-    else
-      valid=yes
-      break $i
-    fi
-  done
-  if [ ! -z $valid ]; then
-    cd $p2fdmr
-    if [ "$(git branch --list Self_Service)" ]; then
-      git checkout Self_Service
-      cp ${mon_path}proxy/* $p2fdmr -r
-      echo 'Self_Service branch already exists, proxy files copied successfully'
-    else
-      git checkout master
-      echo 'Creating new branch: Self_Service'
-      git checkout -b Self_Service
-      cp ${mon_path}proxy/* $p2fdmr -r
-      echo "Proxy files copied into $p2fdmr successfully."
-    fi
-  fi
-fi
-rm -r /var/www/html/
-cp -r /opt/FDMR-Monitor/html/ /var/www/
-
-# Configure database
-read -p 'Do you want to configure Self Service database and create tables? [Y/n]: ' db_conf
-db_conf=${db_conf:-y}
-if [ "${db_conf,,}" == 'y' ]; then
-  db_info () {
-    if [ $1 == 'DB_USERNAME' ]; then
-      stm='Insert database username:'
-      local loop=1
-    elif [  $1 == 'DB_SERVER' ]; then
-      stm='Insert database server host e.g. localhost:'
-      local loop=1
-    elif [ $1 ==  'DB_NAME' ]; then
-      stm='Insert database name:'
-      local loop=1
-    elif [ $1 == 'DB_PASSWORD' ]; then
-      read -p 'Insert database password, for no password leave it blank: ' input
-      local loop=0
-    else
-      echo "invalid value: $1"
-      return
-    fi
-    if [ $loop -eq 1 ]; then
-      for i in {1..3}; do
-        echo -n "${stm} " ; read input
-        if [ -z $input ]; then
-          echo 'invalid value, try again.'
-          if [ $i -eq 3 ]; then
-            echo 'Invalid Value, exiting'
-            exit 1
-          fi
-        else
-          break $i
-        fi
-      done
-    fi
-    sed -i "s/\(^$1 *= *\).*/\1$input/" fdmr-mon.cfg
-  }
-  # Define the keys of the options to modify
-  db_items=('DB_NAME' 'DB_SERVER' 'DB_USERNAME' 'DB_PASSWORD')
-  for i in ${!db_items[@]}; do
-    db_info ${db_items[$i]}
-  done
-  #Create and update database tables
-  python3 mon_db.py --create
-  python3 mon_db.py --update
-fi
-
-# install log rotate file
-  cp /opt/FDMR-Monitor/utils/logrotate/fdmr_mon /etc/logrotate.d/
-  
-# Copy systemd file
-    cp /opt/FDMR-Monitor/utils/systemd/fdmr_mon.service /etc/systemd/system/
-    systemctl daemon-reload
-    systemctl enable fdmr_mon.service
-    
-   # systemctl start fdmr_mon.service
-    
-
-EOF
-
-chmod +x /opt/FDMR-Monitor/install.sh
-sh /opt/FDMR-Monitor/install.sh
 #################
 #sh /opt/FDMR-Monitor/sysinfo/rrd-db.sh
 rm /opt/FDMR-Monitor/sysinfo/*.rrd 
@@ -267,4 +149,4 @@ cp -r /opt/FDMR-Monitor/sysinfo/ /var/www/html/sysinfo/
 sudo rm mon.db
 sudo python3 mon_db.py
 sudo systemctl restart fdmr_mon.service
-#sudo systemctl enable fdmr_mon.service
+sudo systemctl enable fdmr_mon.service
