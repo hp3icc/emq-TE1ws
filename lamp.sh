134ba9eac76984ac2a95d2a715f22273b4010ada
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-06-13 21:44:42 -0500

    Create lamp.sh

---
+++ lamp.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt install -y apache2 apache2-utils
+systemctl restart apache2
+sudo systemctl enable apache2
+sudo apt install mariadb-server mariadb-client -y
+systemctl restart mariadb
+systemctl enable mariadb
+sudo mysql_secure_installation
