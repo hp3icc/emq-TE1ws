04aacd78425fe6daf5aad11782ebe212b0ac099e
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-06-14 23:29:20 -0500

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -2,12 +2,11 @@
 
 sudo apt install mariadb-server php libapache2-mod-php php-zip php-mbstring php-cli php-common php-curl php-xml php-mysql -y
 
-
-systemctl stop apache2
-systemctl disable apache2
-
+sudo apt install apache2 -y
 
 systemctl restart mariadb
 systemctl enable mariadb
 sudo mysql_secure_installation  --host=localhost --port=3306
 
+systemctl stop apache2
+systemctl disable apache2
