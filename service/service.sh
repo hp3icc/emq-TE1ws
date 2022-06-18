c9ad5ade5ab2b858c97e3537a7ac7df5bbbe9ec3
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-06-17 20:31:51 -0500

    Update service.sh

--- service/service.sh
+++ service/service.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
  
-sytemctl stop analog_bridge.service
-sytemctl stop system/mmdvm_bridge.service
-sytemctl stop system/ysfgateway.service
-sytemctl stop system/ysfparrot.service
-sytemctl stop system/nxdngateway.service
-sytemctl stop system/nxdnparrot.service
-sytemctl stop system/p25gateway.service
-sytemctl stop system/p25parrot.service
-sytemctl stop system/quantar_bridge.service
-sytemctl stop system/ircddbgatewayd.service
-sytemctl stop system/md380-emu.service
+sudo systemctl stop analog_bridge.service
+sudo systemctl stop system/mmdvm_bridge.service
+sudo systemctl stop system/ysfgateway.service
+sudo systemctl stop system/ysfparrot.service
+sudo systemctl stop system/nxdngateway.service
+sudo systemctl stop system/nxdnparrot.service
+sudo systemctl stop system/p25gateway.service
+sudo systemctl stop system/p25parrot.service
+sudo systemctl stop system/quantar_bridge.service
+sudo systemctl stop system/ircddbgatewayd.service
+sudo systemctl stop system/md380-emu.service
 #
 rm /lib/systemd/system/analog_bridge.service
 rm /lib/systemd/system/mmdvm_bridge.service
@@ -37,14 +37,14 @@ wget https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/service/ircddbgatew
 wget https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/service/md380-emu.service
 #
 sudo systemctl daemon-reload
-sytemctl start analog_bridge.service
-sytemctl start system/mmdvm_bridge.service
-sytemctl start system/ysfgateway.service
-sytemctl start system/ysfparrot.service
-sytemctl start system/nxdngateway.service
-sytemctl start system/nxdnparrot.service
-sytemctl start system/p25gateway.service
-sytemctl start system/p25parrot.service
-sytemctl start system/quantar_bridge.service
-sytemctl start system/ircddbgatewayd.service
-sytemctl start system/md380-emu.service
+sudo systemctl start analog_bridge.service
+sudo systemctl start system/mmdvm_bridge.service
+sudo systemctl start system/ysfgateway.service
+sudo systemctl start system/ysfparrot.service
+sudo systemctl start system/nxdngateway.service
+sudo systemctl start system/nxdnparrot.service
+sudo systemctl start system/p25gateway.service
+sudo systemctl start system/p25parrot.service
+sudo systemctl start system/quantar_bridge.service
+sudo systemctl start system/ircddbgatewayd.service
+sudo systemctl start system/md380-emu.service
