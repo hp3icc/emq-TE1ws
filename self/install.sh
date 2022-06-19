f47662b949b2f3d063721c13658924a2b5f24167
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-06-19 15:03:41 -0500

    Update install.sh

--- self/install.sh
+++ self/install.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
 
 # Install the required support programs
+cd /opt/FDMR-Monitor/
 apt install python3 python3-pip python3-dev libffi-dev libssl-dev cargo sed \
 default-libmysqlclient-dev build-essential -y
 pip3 install -r requirements.txt
 
-cd /opt/FDMR-Nonitor/
-cp /opt/FDMR-Nonitor/fdmr-mon_SAMPLE.cfg /opt/FDMR-Nonitor/fdmr-mon.cfg
+cd /opt/FDMR-Monitor/
+cp /opt/FDMR-Monitor/fdmr-mon_SAMPLE.cfg /opt/FDMR-Monitor/fdmr-mon.cfg
 cd /opt/FreeDMR/
 rm /opt/FreeDMR/hotspot_proxy_v2.py
 rm /opt/FreeDMR/proxy_db.py
 cp /opt/FDMR-Monitor/proxy/* /opt/FreeDMR/ -r
       
-cd /opt/FDMR-Nonitor/
+cd /opt/FDMR-Monitor/
   
 rm /var/www/html/* -r
 cp /opt/FDMR-Monitor/html/* /var/www/html/ -r
@@ -20,8 +21,8 @@ cp /opt/FDMR-Monitor/html/* /var/www/html/ -r
 chown www-data:www-data /var/www/html/ -R
      
 
-cp /opt/FDMR-Nonitor/utils/logrotate/fdmr_mon /etc/logrotate.d/
-cp /opt/FDMR-Nonitor/utils/systemd/fdmr_mon.service /etc/systemd/system/
+cp /opt/FDMR-Monitor/utils/logrotate/fdmr_mon /etc/logrotate.d/
+cp /opt/FDMR-Monitor/utils/systemd/fdmr_mon.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable fdmr_mon.service
 systemctl start fdmr_mon.service
