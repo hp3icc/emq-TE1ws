acbed7995a4d45cb6c7020898a289c0dbbe3db4e
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-06-21 23:46:36 -0500

    Update list-update.sh

--- list-update.sh
+++ list-update.sh
@@ -199,6 +199,40 @@ exit 0
 EOF
 
 #
+###
+sudo cat > /bin/menu-update <<- "EOF"
+#!/bin/bash
+while : ; do
+choix=$(whiptail --title "Raspbian Proyect HP3ICC Menu FreeDMR" --menu "Nota Importante: solo actualice aplicaciones que esten en uso, al finalizar la actualizacion la aplicacion se reiniciara, tenga pleno dominio de las configuraciones de cada aplicacion, antes de actualizar.
+" 17 50 5 \
+1 " Update FDMR-Monitor OA4DOA " \
+2 " Update FreeDMR  " \
+3 " ADVANCE UPGRADE " \
+4 " Menu Principal " 3>&1 1>&2 2>&3)
+exitstatus=$?
+#on recupere ce choix
+#exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $choix
+else
+    echo "You chose cancel."; break;
+fi
+# case : action en fonction du choix
+case $choix in
+1)
+menu-up-hbmon2 ;;
+2)
+menu-up-fdm ;;
+3)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/list-update.sh)" ;;
+4)
+break;
+esac
+done
+exit 0
+
+EOF
+########
 sudo cat > /tmp/menu-update <<- "EOF"
 #!/bin/bash
 while : ; do
