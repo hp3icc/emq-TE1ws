b49febdfd8ba1f746ec6eefc29c00f2e38649310
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-06-21 23:41:37 -0500

    Update list-updatex.sh

--- list-updatex.sh
+++ list-updatex.sh
@@ -104,10 +104,73 @@ exit 0
 
 EOF
 #
+sudo cat > /bin/menu-fdmr <<- "EOF"
+#!/bin/bash
+while : ; do
+choix=$(whiptail --title "Raspbian Proyect HP3ICC Menu FreeDMR" --menu "Suba o Baje con las flechas del teclado y seleccione el numero de opcion:" 23 56 13 \
+1 " Editar FreeDMR Server " \
+2 " Editar Interlink  " \
+3 " Editar FDMR-Monitor  " \
+4 " cambiar Puerto HTTP FDMR-Monitor " \
+5 " Parrot on  " \
+6 " Parrot off  " \
+7 " Iniciar FreeDMR Server  " \
+8 " Detener FreeDMR Server   " \
+9 " FDMR-Monitor On-Restart " \
+10 " FDMR-Monitor off  " \
+11 " D-APRS Igate  " \
+12 " Actualizar FreeDMR  " \
+13 " Menu Principal " 3>&1 1>&2 2>&3)
+exitstatus=$?
+#on recupere ce choix
+#exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $choix
+else
+    echo "You chose cancel."; break;
+fi
+# case : action en fonction du choix
+case $choix in
+1)
+sudo nano /opt/FreeDMR/config/FreeDMR.cfg ;;
+2)
+sudo nano /opt/FreeDMR/config/rules.py ;;
+3)
+sudo nano /opt/FDMR-Monitor/fdmr-mon.cfg ;;
+4)
+sudo nano /lib/systemd/system/http.server-fmr.service && sudo systemctl daemon-reload ;;
+5)
+sudo systemctl stop fdmrparrot.service && sudo systemctl start fdmrparrot.service && sudo systemctl enable fdmrparrot.service ;;
+6)
+sudo systemctl stop fdmrparrot.service &&  sudo systemctl disable fdmrparrot.service ;;
+7)
+sudo systemctl stop proxy.service && sudo systemctl start proxy.service && sudo systemctl enable proxy.service && sudo systemctl stop freedmr.service && sudo systemctl start freedmr.service && sudo systemctl enable freedmr.service ;;
+8)
+sudo systemctl stop freedmr.service && sudo systemctl disable freedmr.service && sudo systemctl stop proxy.service && sudo systemctl disable proxy.service && rm /var/log/FreeDMR/* ;;
+9)
+echo 123> /opt/FDMR-Monitor/data/123.json && sudo systemctl stop fdmr_mon.service && sudo rm /opt/FDMR-Monitor/data/*.json && sudo rm /opt/FDMR-Monitor/sysinfo/*.rrd && sh /opt/FDMR-Monitor/sysinfo/rrd-db.sh && cronedit.sh '*/5 * * * *' 'sh /opt/FDMR-Monitor/sysinfo/graph.sh' add && cronedit.sh '*/2 * * * *' 'sh /opt/FDMR-Monitor/sysinfo/cpu.sh' add && sudo systemctl enable fdmr_mon.service && sudo systemctl restart apache2.service && sudo systemctl enable apache2.service && sudo systemctl start fdmr_mon.service ;;
+10)
+sudo systemctl stop fdmr_mon.service && sudo systemctl disable fdmr_mon.service && sudo systemctl stop apache2.service && sudo systemctl disable apache2.service && cronedit.sh '*/5 * * * *' 'sh /opt/FDMR-Monitor/sysinfo/graph.sh' remove && cronedit.sh '*/2 * * * *' 'sh /opt/FDMR-Monitor/sysinfo/cpu.sh' remove ;;
+11)
+menu-igate ;;
+12)
+menu-up-fdm ;;
+13)
+break;
+esac
+done
+exit 0
+
+
+
+
+EOF
+#####
+#
 sudo cat > /bin/menu-up-hbmon2 <<- "EOF"
 #!/bin/bash
 while : ; do
-choix=$(whiptail --title "Raspbian Proyect HP3ICC / update HBMon2" --menu "Nota Importante: antes de iniciar la actualizacion, el proceso de actualizacion borrara por completo todas las configuraciones, al finalizar la actualizacion el servicio se reinniciara automaticamente.
+choix=$(whiptail --title "Raspbian Proyect HP3ICC / update FDMR-Monitor OA4DOA" --menu "Nota Importante: antes de iniciar la actualizacion, el proceso de actualizacion borrara por completo todas las configuraciones, al finalizar la actualizacion el servicio se reinniciara automaticamente.
 " 17 50 4 \
 1 " shell extra " \
 2 " Iniciar Actualizacion HBMon2  " \
@@ -125,7 +188,7 @@ case $choix in
 1)
 sudo nano /opt/extra-2.sh && chmod +x /opt/extra* ;;
 2)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/hbmon2-update.sh)" ;;
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/selfservice-update.sh)" ;;
 3)
 break;
 esac
@@ -134,6 +197,7 @@ exit 0
 
 
 EOF
+
 #
 sudo cat > /tmp/menu-update <<- "EOF"
 #!/bin/bash
