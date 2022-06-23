67bb7abe70cccda75f0e3688fa4990eb4cf211dc
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-06-22 23:02:11 -0500

    Update emq-TE1x.sh

--- emq-TE1x.sh
+++ emq-TE1x.sh
@@ -196,7 +196,7 @@ menu-noip ;;
 12)
 menu-web ;;
 13)
-menu-update ;;
+sh -c "$(curl -fsSL https://github.com/hp3icc/emq-TE1ws/raw/main/update.sh)" ;;
 14)
 menu-reboot ;;
 15)
@@ -215,40 +215,7 @@ variable22=$(grep "EMQ-VER:" /opt/emq-ver | tail -c 5)
 sudo sed -i "s/R00ab/$variable22/g"  /bin/menu
 sudo gpsd /dev/ttyACM0 -F /var/run/gpsd.sock
 
-###
-sudo cat > /bin/menu-update <<- "EOF"
-#!/bin/bash
-while : ; do
-choix=$(whiptail --title "Raspbian Proyect HP3ICC Menu FreeDMR" --menu "Nota Importante: solo actualice aplicaciones que esten en uso, al finalizar la actualizacion la aplicacion se reiniciara, tenga pleno dominio de las configuraciones de cada aplicacion, antes de actualizar.
-" 17 50 5 \
-1 " Update FDMR-Monitor OA4DOA " \
-2 " Update FreeDMR  " \
-3 " ADVANCE UPGRADE " \
-4 " Menu Principal " 3>&1 1>&2 2>&3)
-exitstatus=$?
-#on recupere ce choix
-#exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    echo "Your chosen option:" $choix
-else
-    echo "You chose cancel."; break;
-fi
-# case : action en fonction du choix
-case $choix in
-1)
-menu-up-hbmon2 ;;
-2)
-menu-up-fdm ;;
-3)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/list-update.sh)" ;;
-4)
-break;
-esac
-done
-exit 0
-
-EOF
-#################
+####################
 echo iniciando instalacion
 
 cd /opt
@@ -641,75 +608,7 @@ exit 0
 
 
 EOF
-#####
-sudo cat > /bin/menu-up-fdm <<- "EOF"
-#!/bin/bash
-while : ; do
-choix=$(whiptail --title "Raspbian Proyect HP3ICC / update FreeDMR" --menu "Nota Importante: debe debe agregar todos sus obp en la opcion numero uno, ( 1-Lista de OBP )antes de iniciar la actualizacion, el proceso de actualizacion borrara por completo la carpeta /opt/FreeDMR, al finalizar la actualizacion el servicio Freedmr se reinniciara automaticamente.
-" 18 55 5 \
-1 " List OBP " \
-2 " List Rules  " \
-3 " shell extra " \
-4 " Iniciar Actualizacion FreeDMR  " \
-5 " Menu Principal " 3>&1 1>&2 2>&3)
-exitstatus=$?
-#on recupere ce choix
-#exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    echo "Your chosen option:" $choix
-else
-    echo "You chose cancel."; break;
-fi
-# case : action en fonction du choix
-case $choix in
-1)
-sudo nano /opt/obp.txt ;;
-2)
-sudo nano /opt/rules.txt ;;
-3)
-sudo nano /opt/extra-1.sh && chmod +x /opt/extra* ;;
-4)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/fdmr-update.sh)" ;;
-5)
-break;
-esac
-done
-exit 0
-
-
-EOF
-#
-sudo cat > /bin/menu-up-hbmon2 <<- "EOF"
-#!/bin/bash
-while : ; do
-choix=$(whiptail --title "Raspbian Proyect HP3ICC / update FDMR-Monitor OA4DOA" --menu "Nota Importante: antes de iniciar la actualizacion, el proceso de actualizacion borrara por completo todas las configuraciones, al finalizar la actualizacion el servicio se reinniciara automaticamente.
-" 17 50 4 \
-1 " shell extra " \
-2 " Iniciar Actualizacion HBMon2  " \
-3 " Menu Principal " 3>&1 1>&2 2>&3)
-exitstatus=$?
-#on recupere ce choix
-#exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    echo "Your chosen option:" $choix
-else
-    echo "You chose cancel."; break;
-fi
-# case : action en fonction du choix
-case $choix in
-1)
-sudo nano /opt/extra-2.sh && chmod +x /opt/extra* ;;
-2)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/selfservice-update.sh)" ;;
-3)
-break;
-esac
-done
-exit 0
-
-
-EOF
-#
+######
 sudo cat > /opt/obp.txt <<- "EOF"
 #Coloque abajo su lista de obp
 
