60bd41c818bf44efac2e8161179921f47ca031f9
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-06-22 23:20:38 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -33,7 +33,7 @@ exit 0
 
 EOF
 ####
-sudo cat > /tmp/menu-update2 <<- "EOF"
+sudo cat > /bin/menu-update2 <<- "EOF"
 #!/bin/bash
 while : ; do
 choix=$(whiptail --title "Raspbian Proyect HP3ICC Menu FreeDMR" --menu "Nota Importante: solo actualice aplicaciones que esten en uso, al finalizar la actualizacion la aplicacion se reiniciara, tenga pleno dominio de las configuraciones de cada aplicacion, antes de actualizar.
