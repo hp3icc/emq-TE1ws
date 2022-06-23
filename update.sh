894de6b4be75d2c421994fbe016cfb7ef2a30631
hp3icc <49044505+hp3icc@users.noreply.github.com>
2022-06-22 23:09:04 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -63,6 +63,75 @@ exit 0
 
 EOF
 #
+sudo cat > /bin/menu-up-fdm <<- "EOF"
+#!/bin/bash
+while : ; do
+choix=$(whiptail --title "Raspbian Proyect HP3ICC / update FreeDMR" --menu "Nota Importante: debe debe agregar todos sus obp en la opcion numero uno, ( 1-Lista de OBP )antes de iniciar la actualizacion, el proceso de actualizacion borrara por completo la carpeta /opt/FreeDMR, al finalizar la actualizacion el servicio Freedmr se reinniciara automaticamente.
+" 18 55 5 \
+1 " List OBP " \
+2 " List Rules  " \
+3 " shell extra " \
+4 " Iniciar Actualizacion FreeDMR  " \
+5 " Menu Principal " 3>&1 1>&2 2>&3)
+exitstatus=$?
+#on recupere ce choix
+#exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $choix
+else
+    echo "You chose cancel."; break;
+fi
+# case : action en fonction du choix
+case $choix in
+1)
+sudo nano /opt/obp.txt ;;
+2)
+sudo nano /opt/rules.txt ;;
+3)
+sudo nano /opt/extra-1.sh && chmod +x /opt/extra* ;;
+4)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/fdmr-update.sh)" ;;
+5)
+break;
+esac
+done
+exit 0
+
+
+EOF
+#
+sudo cat > /bin/menu-up-hbmon2 <<- "EOF"
+#!/bin/bash
+while : ; do
+choix=$(whiptail --title "Raspbian Proyect HP3ICC / update FDMR-Monitor OA4DOA" --menu "Nota Importante: antes de iniciar la actualizacion, el proceso de actualizacion borrara por completo todas las configuraciones, al finalizar la actualizacion el servicio se reinniciara automaticamente.
+" 17 50 4 \
+1 " shell extra " \
+2 " Iniciar Actualizacion HBMon2  " \
+3 " Menu Principal " 3>&1 1>&2 2>&3)
+exitstatus=$?
+#on recupere ce choix
+#exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $choix
+else
+    echo "You chose cancel."; break;
+fi
+# case : action en fonction du choix
+case $choix in
+1)
+sudo nano /opt/extra-2.sh && chmod +x /opt/extra* ;;
+2)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/selfservice-update.sh)" ;;
+3)
+break;
+esac
+done
+exit 0
+
+
+EOF
+
+#####
 
 
 
