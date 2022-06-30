351780d70acf6a96c7f9755855c28f8bc5f8a827
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-06-30 13:36:32 -0500

    Update fdmr-update.sh

--- fdmr-update.sh
+++ fdmr-update.sh
@@ -106,9 +106,9 @@ sudo sed -i 's/54915/49061/' /opt/FreeDMR/playback.cfg
 cat /opt/rules.txt >> /opt/FreeDMR/config/rules.py
 sed '14 a VALIDATE_SERVER_IDS: True' -i /opt/FreeDMR/config/FreeDMR.cfg
 sed '105 a override_ident_tg:' -i /opt/FreeDMR/config/FreeDMR.cfg
-#sudo rm /opt/FreeDMR/hotspot_proxy_v2.py
+sudo rm /opt/FreeDMR/hotspot_proxy_v2.py
 #sudo rm /opt/FreeDMR/proxy_db.*
-cp /opt/FDMR-Monitor/proxy/proxy* /opt/FreeDMR/ -r
+cp /opt/FDMR-Monitor/proxy/* /opt/FreeDMR/ -r
 
 #####################
 sudo sed -i "s/root/emqte1/g"  /opt/FreeDMR/hotspot_proxy_v2.py
