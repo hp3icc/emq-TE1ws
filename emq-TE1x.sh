355a23db8294ddb379114987fc54184b25ce6ef5
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-07-18 10:48:36 -0500

    Update emq-TE1x.sh

--- emq-TE1x.sh
+++ emq-TE1x.sh
@@ -812,9 +812,9 @@ fi
 # case : action en fonction du choix
 case $choix in
 1)
-sudo systemctl restart analog_bridge.service && sudo systemctl restart mmdvm_bridge.service && sudo systemctl restart nxdngateway.service && sudo systemctl restart p25gateway.service && sudo systemctl restart ysfgateway.service && sudo systemctl enable analog_bridge.service && sudo systemctl enable mmdvm_bridge.service && sudo systemctl enable nxdngateway.service && sudo systemctl enable p25gateway.service && sudo systemctl enable ysfgateway.service ;;
+sudo systemctl restart quantar_bridge.service && sudo systemctl enable quantar_bridge.service && sudo systemctl restart p25parrot.service && sudo systemctl enable p25parrot.service && sudo systemctl restart ysfparrot.service && sudo systemctl enable ysfparrot.service && sudo systemctl restart nxdnparrot.service && sudo systemctl enable nxdnparrot.service && sudo systemctl restart ircddbgatewayd.service && sudo systemctl enable ircddbgatewayd.service && sudo systemctl restart analog_bridge.service && sudo systemctl restart mmdvm_bridge.service && sudo systemctl restart nxdngateway.service && sudo systemctl restart p25gateway.service && sudo systemctl restart ysfgateway.service && sudo systemctl enable analog_bridge.service && sudo systemctl enable mmdvm_bridge.service && sudo systemctl enable nxdngateway.service && sudo systemctl enable p25gateway.service && sudo systemctl enable ysfgateway.service ;;
 2)
-sudo systemctl stop analog_bridge.service && sudo systemctl stop mmdvm_bridge.service && sudo systemctl stop nxdngateway.service && sudo systemctl stop p25gateway.service && sudo systemctl stop ysfgateway.service && sudo systemctl disable analog_bridge.service && sudo systemctl disable mmdvm_bridge.service && sudo systemctl disable nxdngateway.service && sudo systemctl disable p25gateway.service && sudo systemctl disable ysfgateway.service && rm /var/log/mmdvm/*;;
+sudo systemctl stop quantar_bridge.service && sudo systemctl disable quantar_bridge.service && sudo systemctl stop p25parrot.service && sudo systemctl disable p25parrot.service && sudo systemctl stop ysfparrot.service && sudo systemctl disable ysfparrot.service && sudo systemctl stop nxdnparrot.service && sudo systemctl disable nxdnparrot.service && sudo systemctl stop ircddbgatewayd.service && sudo systemctl disable ircddbgatewayd.service && sudo systemctl stop analog_bridge.service && sudo systemctl stop mmdvm_bridge.service && sudo systemctl stop nxdngateway.service && sudo systemctl stop p25gateway.service && sudo systemctl stop ysfgateway.service && sudo systemctl disable analog_bridge.service && sudo systemctl disable mmdvm_bridge.service && sudo systemctl disable nxdngateway.service && sudo systemctl disable p25gateway.service && sudo systemctl disable ysfgateway.service && rm /var/log/mmdvm/*;;
 3)
 sudo /usr/local/dvs/dvs ;;
 4)
@@ -2371,16 +2371,29 @@ chmod +x /opt/YSF2DMR/DMRIDUpdate2.sh
 
 sudo systemctl disable lighttpd.service
 sudo systemctl stop lighttpd.service
+
 sudo systemctl stop analog_bridge.service
 sudo systemctl stop mmdvm_bridge.service
 sudo systemctl stop nxdngateway.service
 sudo systemctl stop p25gateway.service
 sudo systemctl stop ysfgateway.service
+sudo systemctl stop quantar_bridge.service
+sudo systemctl stop ircddbgatewayd.service
+sudo systemctl stop p25parrot.service
+sudo systemctl stop ysfparrot.service
+sudo systemctl stop nxdnparrot.service
+
 sudo systemctl disable analog_bridge.service
 sudo systemctl disable mmdvm_bridge.service
 sudo systemctl disable nxdngateway.service
 sudo systemctl disable p25gateway.service
 sudo systemctl disable ysfgateway.service
+sudo systemctl disable quantar_bridge.service
+sudo systemctl disable ircddbgatewayd.service
+sudo systemctl disable p25parrot.service
+sudo systemctl disable ysfparrot.service
+sudo systemctl disable nxdnparrot.service
+
 rm /var/log/mmdvm/*
 
 ###########################
