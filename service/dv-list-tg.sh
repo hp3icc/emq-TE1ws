4db6f9c1e4015785e7cb10e16d895a608e78bd3b
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-07-27 22:34:46 -0500

    Update dv-list-tg.sh

--- service/dv-list-tg.sh
+++ service/dv-list-tg.sh
@@ -1,9 +1,9 @@
 sudo cat > /bin/menu-update <<- "EOF"
 #!/bin/bash
 while : ; do
-choix=$(whiptail --title "Raspbian Proyect HP3ICC Menu TG List DVSwitch" --menu "Suba o Baje con las flechas del teclado y seleccione el numero de opcion, al finalizar debe actualizar la lista de tg en su DVSwitch Mobile. " 17 50 5 \
-1 " List TG FreeDMR " \
-2 " List TG Brandmeister  " \
+choix=$(whiptail --title "Raspbian Proyect HP3ICC Menu TG List DVSwitch" --menu "Suba o Baje con las flechas del teclado y seleccione presionando intro, al finalizar debe actualizar la lista de tg en su DVSwitch Mobile. " 17 50 5 \
+1 " TG list FreeDMR " \
+2 " TG list Brandmeister  " \
 3 " Menu Principal " 3>&1 1>&2 2>&3)
 exitstatus=$?
 #on recupere ce choix
@@ -16,9 +16,9 @@ fi
 # case : action en fonction du choix
 case $choix in
 1)
-sudo sed -i "s/www.pistar.uk/freedmr-hp.ddns.net/g"  /opt/MMDVM_Bridge/dvswitch.sh ;;
+sudo sed -i "s/www.pistar.uk/freedmr-hp.ddns.net/g"  /opt/MMDVM_Bridge/dvswitch.sh && sh /usr/local/sbin/DVSM_Update.sh ;;
 2)
-sudo sed -i "s/freedmr-hp.ddns.net/www.pistar.uk/g"  /opt/MMDVM_Bridge/dvswitch.sh ;;
+sudo sed -i "s/freedmr-hp.ddns.net/www.pistar.uk/g"  /opt/MMDVM_Bridge/dvswitch.sh && sh /usr/local/sbin/DVSM_Update.sh ;;
 3)
 break;
 esac
