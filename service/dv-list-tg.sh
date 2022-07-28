464c91d2e1301a9474be266537dcf31bb04d192e
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-07-28 01:28:04 -0500

    Update dv-list-tg.sh

--- service/dv-list-tg.sh
+++ service/dv-list-tg.sh
@@ -1,7 +1,7 @@
 sudo cat > /bin/menu-update-tg <<- "EOF"
 #!/bin/bash
 while : ; do
-choix=$(whiptail --title "Raspbian Proyect HP3ICC Menu TG List DVSwitch" --menu "Suba o Baje con las flechas del teclado y seleccione presionando intro, al finalizar debe actualizar la lista de tg en su DVSwitch Mobile. " 18 50 5 \
+choix=$(whiptail --title "Raspbian Proyect HP3ICC Menu TG List DVSwitch" --menu "Suba o Baje con las flechas del teclado y seleccione presionando intro, al finalizar debe actualizar la lista de tg en su DVSwitch Mobile. " 18 55 5 \
 1 " TG list FreeDMR " \
 2 " TG list Brandmeister  " \
 3 " Menu Principal " 3>&1 1>&2 2>&3)
@@ -30,7 +30,7 @@ EOF
 sudo cat > /bin/menu-update-tg2 <<- "EOF"
 #!/bin/bash
 while : ; do
-choix=$(whiptail --title "Raspbian Proyect HP3ICC Menu TG List DVSwitch" --menu "Proceda a actualizar su DVSwitch Mobile. " 17 50 5 \
+choix=$(whiptail --title "Raspbian Proyect HP3ICC Menu TG List DVSwitch" --menu "Proceda a actualizar su DVSwitch Mobile. " 17 55 5 \
 1 " New TG List Ready " 3>&1 1>&2 2>&3)
 exitstatus=$?
 #on recupere ce choix
