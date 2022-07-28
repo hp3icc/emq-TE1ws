1018e0557d77af07c23ed3529ce1dfd2b5fff802
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-07-28 01:20:00 -0500

    Update dv-list-tg.sh

--- service/dv-list-tg.sh
+++ service/dv-list-tg.sh
@@ -1,4 +1,4 @@
-sudo cat > /bin/menu-update <<- "EOF"
+sudo cat > /bin/menu-update-tg <<- "EOF"
 #!/bin/bash
 while : ; do
 choix=$(whiptail --title "Raspbian Proyect HP3ICC Menu TG List DVSwitch" --menu "Suba o Baje con las flechas del teclado y seleccione presionando intro, al finalizar debe actualizar la lista de tg en su DVSwitch Mobile. " 17 50 5 \
@@ -16,9 +16,9 @@ fi
 # case : action en fonction du choix
 case $choix in
 1)
-sudo sed -i "s/www.pistar.uk/freedmr-hp.ddns.net/g" /opt/MMDVM_Bridge/dvswitch.sh  ;;
+sudo sed -i "s/www.pistar.uk/freedmr-hp.ddns.net/g" /opt/MMDVM_Bridge/dvswitch.sh  && menu-update-tg2 ;;
 2)
-sudo sed -i "s/freedmr-hp.ddns.net/www.pistar.uk/g" /opt/MMDVM_Bridge/dvswitch.sh  ;;
+sudo sed -i "s/freedmr-hp.ddns.net/www.pistar.uk/g" /opt/MMDVM_Bridge/dvswitch.sh  && menu-update-tg2 ;;
 3)
 /usr/local/dvs/dvs;
 esac
@@ -27,5 +27,27 @@ exit 0
 
 EOF
 ####
-chmod +x /bin/menu-update
-/bin/menu-update
+sudo cat > /bin/menu-update-tg2 <<- "EOF"
+#!/bin/bash
+while : ; do
+choix=$(whiptail --title "Raspbian Proyect HP3ICC Menu TG List DVSwitch" --menu "Proceda a actualizar su DVSwitch Mobile. " 17 50 5 \
+1 " New TG List Ready " 3>&1 1>&2 2>&3)
+exitstatus=$?
+#on recupere ce choix
+#exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $choix
+else
+    echo "You chose cancel."; break;
+fi
+# case : action en fonction du choix
+case $choix in
+1)
+/usr/local/dvs/dvs;
+esac
+done
+exit 0
+EOF
+####
+chmod +x /bin/menu*
+/bin/menu-update-tg
