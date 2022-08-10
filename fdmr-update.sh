83604342fd98e920aa2954bc238f04cf6afe4cf1
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-08-10 06:40:18 -0500

    Update fdmr-update.sh

--- fdmr-update.sh
+++ fdmr-update.sh
@@ -99,7 +99,7 @@ sudo sed -i 's/100/60/' /opt/FreeDMR/config/FreeDMR.cfg
 #sudo sed -i 's/REPORT_CLIENTS: 127.0.0.1/REPORT_CLIENTS: */' /opt/FreeDMR/config/FreeDMR.cfg
 sudo sed -i "s/TGID_URL:/#TGID_URL:/g"  /opt/FreeDMR/config/FreeDMR.cfg 
 sed '37 a TGID_URL: https://freedmr.cymru/talkgroups/talkgroup_ids_json.php' -i /opt/FreeDMR/config/FreeDMR.cfg
-sed '43 a TOPO_FILE:' -i /opt/FreeDMR/config/FreeDMR.cfg 
+sed '43 a TOPO_FILE: topography.json' -i /opt/FreeDMR/config/FreeDMR.cfg 
 rm /opt/conf.txt
 #rm /opt/obp.txt
 cd /opt/FreeDMR/
