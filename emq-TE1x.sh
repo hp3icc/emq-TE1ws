db00ee73f5ebeab522b5826a2e1a55ac0928641e
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-08-19 20:56:49 -0500

    Update emq-TE1x.sh

--- emq-TE1x.sh
+++ emq-TE1x.sh
@@ -79,7 +79,7 @@ sudo apt-get install rrdtool -y
 
 ##################
 sudo cat > /opt/emq-ver <<- "EOF"
-EMQ-VER:  20.2
+EMQ-VER:  20.3
 EOF
 #########
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/menu.sh)"
