d410126b7d66a078ade68c414cac1910ba1580f2
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-11-26 02:23:13 -0500

    Update install.sh

--- self/install.sh
+++ self/install.sh
@@ -13,7 +13,7 @@ sudo systemctl stop proxy.service
 #sudo rm /opt/FreeDMR/hotspot_proxy_v2.py
 sudo rm /opt/FreeDMR/proxy_db.*
 #cp /opt/FDMR-Monitor/proxy/* /opt/FreeDMR/ -r
-#cp /opt/FDMR-Monitor/proxy/hotspot_proxy_v2.py /opt/FreeDMR/hotspot_proxy_v2.py -r
+cp /opt/FDMR-Monitor/proxy/hotspot_proxy_v2.py /opt/FreeDMR/hotspot_proxy_v2.py -r
 cp /opt/FDMR-Monitor/proxy/proxy.cfg /opt/FreeDMR/proxy.cfg -r
 cp /opt/FDMR-Monitor/proxy/proxy_db.py /opt/FreeDMR/proxy_db.py -r
       
