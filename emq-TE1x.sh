414fd44fd790b4ae92c396fd3c6ea293f83adb25
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-12-01 14:33:51 -0500

    Update emq-TE1x.sh

--- emq-TE1x.sh
+++ emq-TE1x.sh
@@ -433,10 +433,12 @@ cat > /lib/systemd/system/monp.service  <<- "EOF"
 Description=sudo modprobe i2c-dev
 #Wants=network-online.target
 #After=syslog.target network-online.target
+
 [Service]
 User=root
 #ExecStartPre=/bin/sleep 1800
 ExecStart=sudo modprobe i2c-dev
+
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -540,6 +542,7 @@ User=root
 #ExecStartPre=/bin/sleep 30
 # Modify for different other port
 ExecStart=php -S 0.0.0.0:80 -t /var/www/dvs/
+
 [Install]
 WantedBy=multi-user.target
 
@@ -573,6 +576,7 @@ cat > /lib/systemd/system/ionos.service <<- "EOF"
 Description=Ionphere-RTL Service
 Wants=network-online.target
 After=syslog.target network-online.target
+
 [Service]
 User=root
 Type=simple
@@ -585,6 +589,7 @@ ExecStartPre=/bin/sh -c 'until ping -c1 noip.com; do sleep 1; done;'
 ExecStart=sh /opt/ionsphere/ionosphere-raspberry-pi/ionos.sh
 ExecReload=/bin/kill -HUP $MAINPID
 KillMode=process
+
 [Install]
 # To make the network-online.target available
 # systemctl enable systemd-networkd-wait-online.service
@@ -2313,34 +2318,34 @@ sudo chmod +x /opt/FreeDMR/install.sh
 sudo chmod +x /opt/FreeDMR/*.py
 sudo cat > /opt/conf.txt <<- "EOF"
  
-[D-APRS]
+ [D-APRS]
 MODE: MASTER
 ENABLED: True
-REPEAT: True
+REPEAT: False
 MAX_PEERS: 1
 EXPORT_AMBE: False
 IP:
-PORT: 54101
-PASSPHRASE: passw@rd
-GROUP_HANGTIME: 5
+PORT: 52555
+PASSPHRASE:
+GROUP_HANGTIME: 0
 USE_ACL: True
 REG_ACL: DENY:1
 SUB_ACL: DENY:1
 TGID_TS1_ACL: PERMIT:ALL
 TGID_TS2_ACL: PERMIT:ALL
-DEFAULT_UA_TIMER: 60
-SINGLE_MODE: True
+DEFAULT_UA_TIMER: 10
+SINGLE_MODE: False
 VOICE_IDENT: False
 TS1_STATIC:
 TS2_STATIC:
 DEFAULT_REFLECTOR: 0
 ANNOUNCEMENT_LANGUAGE: es_ES
-GENERATOR: 1
+GENERATOR: 2
 ALLOW_UNREG_ID: True
 PROXY_CONTROL: False
-override_ident_tg:
+OVERRIDE_IDENT_TG:
  
-[LINKS]
+ [LINKS]
 MODE: MASTER
 ENABLED: True
 REPEAT: True
@@ -2348,7 +2353,7 @@ MAX_PEERS: 5
 EXPORT_AMBE: False
 IP:
 PORT: 54102
-PASSPHRASE: passw@rd
+PASSPHRASE: passw0rd
 GROUP_HANGTIME: 5
 USE_ACL: True
 REG_ACL: DENY:1
@@ -2367,7 +2372,7 @@ ALLOW_UNREG_ID: True
 PROXY_CONTROL: False
 override_ident_tg:
  
-[EchoTest]
+ [EchoTest]
 MODE: PEER
 ENABLED: True
 LOOSE: True
@@ -2409,11 +2414,9 @@ DEFAULT_UA_TIMER: 999
 SINGLE_MODE: True
 VOICE_IDENT: False
  
+ 
 EOF
 ##
-sudo sed -i 's/ALLOW_NULL_PASSPHRASE: True/ALLOW_NULL_PASSPHRASE: False/' /opt/FreeDMR/FreeDMR-SAMPLE.cfg
-sudo sed -i 's/PASSPHRASE:/PASSPHRASE: passw0rd/' /opt/FreeDMR/FreeDMR-SAMPLE.cfg
-sudo sed -i 's/ALLOW_NULL_PASSPHRASE: passw0rd False/ALLOW_NULL_PASSPHRASE: False/' /opt/FreeDMR/FreeDMR-SAMPLE.cfg
 
 cp /opt/FreeDMR/FreeDMR-SAMPLE.cfg /opt/
 cd /opt/
@@ -2656,14 +2659,19 @@ sudo sed -i "s/PRIVATE_NETWORK = True/PRIVATE_NETWORK = False/g"  /opt/FDMR-Moni
 sudo sed -i "s/TGID_URL =/#TGID_URL =/g"  /opt/FDMR-Monitor/fdmr-mon_SAMPLE.cfg
 sed '63 a TGID_URL = https://freedmr.cymru/talkgroups/talkgroup_ids_json.php' -i /opt/FDMR-Monitor/fdmr-mon_SAMPLE.cfg
 sed '64 a #TGID_URL = https://freedmr.cymru/talkgroups/talkgroup_ids_flags_json.php' -i /opt/FDMR-Monitor/fdmr-mon_SAMPLE.cfg
-sudo rm /opt/FDMR-Monitor/data/*
 cd /opt/FDMR-Monitor/
 sudo rm /opt/FDMR-Monitor/install.sh
 wget https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/self/install.sh
 sudo chmod +x /opt/FDMR-Monitor/install.sh
 #
 sh /opt/FDMR-Monitor/install.sh
-sudo sh /opt/extra-2.sh
+sudo cat > /bin/data-id <<- "EOF"
+#!/bin/bash
+wget /opt/FDMR-Monitor/data/talkgroup_ids.json https://freedmr.cymru/talkgroups/talkgroup_ids_json.php -O
+wget /opt/FDMR-Monitor/data/subscriber_ids.csv https://database.radioid.net/static/user.csv -O
+wget /opt/FDMR-Monitor/data/peer_ids.json https://database.radioid.net/static/rptrs.json -O
+EOF
+chmod +x /bin/data-id
 #####################
 sudo sed -i "s/root/emqte1/g"  /opt/FreeDMR/hotspot_proxy_v2.py
 sudo sed -i "s/54100/54060/g"  /opt/FreeDMR/hotspot_proxy_v2.py
@@ -2688,49 +2696,13 @@ systemctl stop apache2
 systemctl disable apache2
 #########
 sudo cat > /opt/FreeDMR/config/rules.py <<- "EOF"
-'''
-THIS EXAMPLE WILL NOT WORK AS IT IS - YOU MUST SPECIFY YOUR OWN VALUES!!!
-
-In FreeDMR, the rules file should be *empty* unless you have static routing required. Please see the 
-documentation for more details.
-
-This file is organized around the "Conference Bridges" that you wish to use. If you're a c-Bridge
-person, think of these as "bridge groups". You might also liken them to a "reflector". If a particular
-system is "ACTIVE" on a particular conference bridge, any traffid from that system will be sent
-to any other system that is active on the bridge as well. This is not an "end to end" method, because
-each system must independently be activated on the bridge.
-
-The first level (e.g. "WORLDWIDE" or "STATEWIDE" in the examples) is the name of the conference
-bridge. This is any arbitrary ASCII text string you want to use. Under each conference bridge
-definition are the following items -- one line for each HBSystem as defined in the main HBlink
-configuration file.
-
-    * SYSTEM - The name of the sytem as listed in the main hblink configuration file (e.g. hblink.cfg)
-        This MUST be the exact same name as in the main config file!!!
-    * TS - Timeslot used for matching traffic to this confernce bridge
-        XLX connections should *ALWAYS* use TS 2 only.
-    * TGID - Talkgroup ID used for matching traffic to this conference bridge
-        XLX connections should *ALWAYS* use TG 9 only.
-    * ON and OFF are LISTS of Talkgroup IDs used to trigger this system off and on. Even if you
-        only want one (as shown in the ON example), it has to be in list format. None can be
-        handled with an empty list, such as " 'ON': [] ".
-    * TO_TYPE is timeout type. If you want to use timers, ON means when it's turned on, it will
-        turn off afer the timout period and OFF means it will turn back on after the timout
-        period. If you don't want to use timers, set it to anything else, but 'NONE' might be
-        a good value for documentation!
-    * TIMOUT is a value in minutes for the timout timer. No, I won't make it 'seconds', so don't
-        ask. Timers are performance "expense".
-    * RESET is a list of Talkgroup IDs that, in addition to the ON and OFF lists will cause a running
-        timer to be reset. This is useful   if you are using different TGIDs for voice traffic than
-        triggering. If you are not, there is NO NEED to use this feature.
-'''
-
 BRIDGES = {
 
  '9990': [ 
 	{'SYSTEM': 'EchoTest', 		'TS': 2, 'TGID': 9990, 'ACTIVE':True, 'TIMEOUT': 0, 'TO_TYPE': 'NONE', 'ON': [], 'OFF': [], 'RESET': []}, 
 
 	],
+
 
 
 }
@@ -2741,8 +2713,7 @@ if __name__ == '__main__':
 EOF
 #######
 sudo cat > /opt/rules.txt <<- "EOF"
- 
-BRIDGES = {
+ BRIDGES = {
  
  '9990': [ 
 {'SYSTEM': 'EchoTest',          'TS': 2, 'TGID': 9990, 'ACTIVE':True, 'TIMEOUT': 0, 'TO_TYPE': 'NONE', 'ON': [], 'OFF': [], 'RESET': []}, 
