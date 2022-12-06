d1aedd4ca11855283354cb7d3e28f6ddf17145bd
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-12-05 20:15:34 -0500

    Update selfservice-update.sh

--- selfservice-update.sh
+++ selfservice-update.sh
@@ -142,9 +142,9 @@ sh /opt/FDMR-Monitor/install.sh
 #
 sudo cat > /bin/data-id <<- "EOF"
 #!/bin/bash
-wget /opt/FDMR-Monitor/data/talkgroup_ids.json https://freedmr.cymru/talkgroups/talkgroup_ids_json.php -O
-wget /opt/FDMR-Monitor/data/subscriber_ids.csv https://database.radioid.net/static/user.csv -O
-wget /opt/FDMR-Monitor/data/peer_ids.json https://database.radioid.net/static/rptrs.json -O
+wget https://freedmr.cymru/talkgroups/talkgroup_ids_json.php -O /opt/FDMR-Monitor/data/talkgroup_ids.json
+wget https://database.radioid.net/static/user.csv -O /opt/FDMR-Monitor/data/subscriber_ids.csv
+wget https://database.radioid.net/static/rptrs.json -O /opt/FDMR-Monitor/data/peer_ids.json
 EOF
 chmod +x /bin/data-id
 data-id
