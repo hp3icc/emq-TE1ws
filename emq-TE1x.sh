2797c08f686c094c9b7a3dec9d0c1178fe4faae6
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-12-07 20:25:58 -0500

    Update emq-TE1x.sh

--- emq-TE1x.sh
+++ emq-TE1x.sh
@@ -86,8 +86,7 @@ EMQ-VER:  20.9
 EOF
 #########
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/menu.sh)"
-variable22=$(grep "EMQ-VER:" /opt/emq-ver | tail -c 5)
-sudo sed -i "s/R00ab/$variable22/g"  /bin/menu
+
 ########################
 sudo cat > /opt/obp.txt <<- "EOF"
  
