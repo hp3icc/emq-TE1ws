0837e5f545ef5b5969f7e12b088d91c9ddd7aad5
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2023-01-31 23:22:50 -0500

    Update emq-TE1x.sh

--- emq-TE1x.sh
+++ emq-TE1x.sh
@@ -2566,102 +2566,7 @@ commands="CREATE DATABASE \`${newDb}\`;CREATE USER '${newUser}'@'${host}' IDENTI
 echo "${commands}" | /usr/bin/mysql -u root
 #fdmr-monitor
 #fdmr-monitor
-cd /opt
-sudo git clone https://github.com/hp3icc/FDMR-Monitor.git
-cd FDMR-Monitor
-sudo git checkout Self_Service
-sudo sed -i 's/RELOAD_TIME = 15/RELOAD_TIME = 1/' /opt/FDMR-Monitor/fdmr-mon_SAMPLE.cfg
-sudo sed -i 's/FREQUENCY = 10/FREQUENCY = 120/' /opt/FDMR-Monitor/fdmr-mon_SAMPLE.cfg
-sudo chmod 644 /opt/FDMR-Monitor/fdmr-mon_SAMPLE.cfg
-sed '33 a <!--' -i /opt/FDMR-Monitor/html/sysinfo.php
-sed '35 a -->' -i /opt/FDMR-Monitor/html/sysinfo.php
-####
-sudo sed -i 's/localhost_2-day.png/localhost_1-day.png/' /opt/FDMR-Monitor/html/sysinfo.php
-sudo sed -i "s/HBMonv2/FDMR-Monitor/g"  /opt/FDMR-Monitor/sysinfo/*.sh
-sudo chmod +x /opt/FDMR-Monitor/sysinfo/cpu.sh
-sudo chmod +x /opt/FDMR-Monitor/sysinfo/graph.sh
-sudo chmod +x /opt/FDMR-Monitor/sysinfo/rrd-db.sh
-#
-sudo cat > /opt/FDMR-Monitor/html/buttons.php <<- "EOF"
-<!-- HBMonitor buttons HTML code -->
-<a class="button" href="index.php">Home</a>
-&nbsp;
-<div class="dropdown">
-  <button class="dropbtn">Links</button>
-  <div class="dropdown-content">
-&nbsp;
-<a class="button" href="linkedsys.php">Linked Systems</a>
-<a class="button" href="statictg.php">Static TG</a>
-<a class="button" href="opb.php">OpenBridge</a>
-&nbsp;
-</div>
-</div>
-<div class="dropdown">
-  <button class="dropbtn">Self Service</button>
-  <div class="dropdown-content">
-    <?php if(!PRIVATE_NETWORK){echo '<a class="button" href="selfservice.php">SelfService</a>';}?>
-    <a class="button" href="login.php">Login</a>
-    <?php 
-    if(isset($_SESSION["auth"], $_SESSION["callsign"], $_SESSION["h_psswd"]) and $_SESSION["auth"]){
-      echo '<a class="button" href="devices.php">Devices</a>';
-    }
-    ?>
-  </div>
-</div>
-<div class="dropdown">
-  <button class="dropbtn">Local Server</button>
-  <div class="dropdown-content">
-<a class="button" href="moni.php">&nbsp;Monitor&nbsp;</a>
-&nbsp;
-<a class="button" href="sysinfo.php">&nbsp;System Info&nbsp;</a>
-&nbsp;
-<a class="button" href="log.php">&nbsp;Lastheard&nbsp;</a>
-&nbsp;
-<a class="button" href="tgcount.php">&nbsp;TG Count&nbsp;</a>
-&nbsp;
-</div>
-</div>
-<div class="dropdown">
-  <button class="dropbtn">FreeDMR</button>
-  <div class="dropdown-content">
-&nbsp;
-<a class="button" href="http://www.freedmr.uk/index.php/why-use-freedmr/"target="_blank">&nbsp;Info FreeDMR&nbsp;</a>
-&nbsp;
-<a class="button" href="http://www.freedmr.uk/index.php/freedmr-servers/"target="_blank">&nbsp;Info Server&nbsp;</a>
-&nbsp;
-<a class="button" href="http://www.freedmr.uk/server_test.php"target="_blank">&nbsp;Status Server&nbsp;</a>
-&nbsp;
-<a class="button" href="http://www.freedmr.uk/index.php/world-wide-talk-groups/"target="_blank">&nbsp;World Wide Talk Groups&nbsp;</a>
-&nbsp;
-<a class="button" href="http://www.freedmr.uk/freedmr/option-calculator-b.php"target="_blank">&nbsp;Static TG Calculator&nbsp;</a>
-&nbsp;
-</div>
-</div>
-<!--
-<a class="button" href="bridges.php">Bridges</a>
--->
-<!-- Example of buttons dropdown HTML code -->
-<!--
-<div class="dropdown">
-  <button class="dropbtn">Admin Area</button>
-  <div class="dropdown-content">
-    <a href="masters.php">Master&Peer</a>
-    <a href="opb.php">OpenBridge</a>
-    <a href="moni.php">Monitor</a>
-  </div>
-</div>
-<div class="dropdown">
-  <button class="dropbtn">Reflectors</button>
-  <div class="dropdown-content">
-    <a target='_blank' href="#">YSF Reflector</a>
-    <a target='_blank' href="#">XLX950</a>
-  </div>
-</div>
--->
-EOF
-
-#
-sudo systemctl daemon-reload
+#######
 cd /opt
 sudo git clone https://github.com/hp3icc/FDMR-Monitor.git
 cd FDMR-Monitor
@@ -2798,7 +2703,7 @@ sudo sed -i "s/test/selfcare/g"  /opt/FreeDMR/proxy.cfg
 #sh /opt/FDMR-Monitor/sysinfo/rrd-db.sh
 #rm /opt/FDMR-Monitor/sysinfo/*.rrd 
 sh /opt/FDMR-Monitor/sysinfo/rrd-db.sh
-#####################
+#######################################
 systemctl stop apache2
 systemctl disable apache2
 #########
