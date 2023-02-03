bba956364addd7942fbd272893d926d99a84f12c
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2023-02-02 23:44:27 -0500

    Update emq-TE1x.sh

--- emq-TE1x.sh
+++ emq-TE1x.sh
@@ -1694,7 +1694,7 @@ sudo chmod +x buster
 apt-get update -y
 apt-get install dvswitch-server -y
 #############
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/menu.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/dvs-config.sh)"
 ########
 sudo cat > /usr/local/dvs/dvs <<- "EOF"
 #!/bin/bash
