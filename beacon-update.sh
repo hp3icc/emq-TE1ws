12e50ead6ae0d0a0887d8bc13898ca8eb53e66f8
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2023-02-07 09:29:23 -0500

    Update beacon-update.sh

--- beacon-update.sh
+++ beacon-update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/shell-aprs/main/shell.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/menu.sh)"
-variable22=$(grep "EMQ-VER:" /opt/emq-ver | tail -c 5)
-sudo sed -i "s/R00ab/$variable22/g"  /bin/menu
+
