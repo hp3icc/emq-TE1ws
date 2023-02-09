6bf1a13bd759602accb7bfcb51936c45fc6f3755
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2023-02-09 14:14:21 -0500

    Update data-id-update.sh

--- self/data-id-update.sh
+++ self/data-id-update.sh
@@ -1,10 +1,10 @@
 cat > /bin/data-id <<- "EOF"
 #!/bin/bash
-# wget https://freedmr.cymru/talkgroups/talkgroup_ids_json.php -O /opt/FDMR-Monitor/data/talkgroup_ids.json
+wget https://freedmr.cymru/talkgroups/talkgroup_ids_json.php -O /opt/FDMR-Monitor/data/talkgroup_ids.json
 wget https://database.radioid.net/static/user.csv -O /opt/FDMR-Monitor/data/subscriber_ids.csv
 wget https://database.radioid.net/static/rptrs.json -O /opt/FDMR-Monitor/data/peer_ids.json
-# wget https://freedmr.cymru/talkgroups/talkgroup_ids_json.php -O /opt/FreeDMR/talkgroup_ids.json
-# wget https://freedmr.cymru/talkgroups/users.json -O /opt/FreeDMR/subscriber_ids.csv
+wget https://freedmr.cymru/talkgroups/talkgroup_ids_json.php -O /opt/FreeDMR/talkgroup_ids.json
+wget https://freedmr.cymru/talkgroups/users.json -O /opt/FreeDMR/subscriber_ids.csv
 wget https://database.radioid.net/static/rptrs.json -O /opt/FreeDMR/peer_ids.json
 EOF
 #
