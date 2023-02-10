eb163f2598d1bd2438c8f2ea275563ec7a282c7d
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2023-02-10 17:12:21 -0500

    Update selfservice-update.sh

--- selfservice-update.sh
+++ selfservice-update.sh
@@ -197,8 +197,7 @@ sh /opt/FDMR-Monitor/sysinfo/rrd-db.sh
 
 #cp -r /opt/FDMR-Monitor/sysinfo/ /var/www/html/sysinfo/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/menu/menu-fdmr)"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/hp3icc/emq-TE1ws/main/self/data-id-update.sh)"
-data-id
+cronedit.sh '* */12 * * *' 'data-id' remove
 #sudo python3 mon_db.py
 sudo systemctl daemon-reload
 sudo systemctl start proxy.service
